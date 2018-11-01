1ae776c4f76816c3909538117c58b66e7b9704cc
Matt Ford <mateoford@gmail.com>
2018-11-01 10:53:11 -0500

    Add 'simple' theme as a framework

---
+++ content/pages/howto.md
@@ -0,0 +1,7 @@
+Title: Instructions
+Authors: Matthew Ford
+Slug: about
+
+# How to use the Bicycle Wheel App
+
+Here is some text
--- pelicanconf.py
+++ pelicanconf.py
@@ -12,6 +12,8 @@ TIMEZONE = 'America/Chicago'
 
 DEFAULT_LANG = u'English'
 
+THEME = "simple"
+
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
@@ -20,10 +22,7 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+LINKS = None
 
 # Social widget
 SOCIAL = (('You can add links in your config file', '#'),
@@ -32,4 +31,4 @@ SOCIAL = (('You can add links in your config file', '#'),
 DEFAULT_PAGINATION = False
 
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+RELATIVE_URLS = True
