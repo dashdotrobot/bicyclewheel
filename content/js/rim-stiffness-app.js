4966f4d12e97f5034b342167afb5964b0702ec7f
Matt Ford <mateoford@gmail.com>
2019-02-21 17:55:47 -0600

    Proof-of-concept record and playback

--- content/js/rim-stiffness-app.js
+++ content/js/rim-stiffness-app.js
@@ -1,20 +1,71 @@
+const recordAudio = () =>
+  new Promise(async resolve => {
+    const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+    const mediaRecorder = new MediaRecorder(stream);
+    const audioChunks = [];
 
-var ctx;
-var buffer;
+    mediaRecorder.addEventListener("dataavailable", event => {
+      audioChunks.push(event.data);
+    });
 
+    const start = () => mediaRecorder.start();
 
+    const stop = () =>
+      new Promise(resolve => {
+        mediaRecorder.addEventListener("stop", () => {
+          const audioBlob = new Blob(audioChunks);
+          const audioUrl = URL.createObjectURL(audioBlob);
+          const audio = new Audio(audioUrl);
+          const play = () => audio.play();
+          resolve({ audioBlob, audioUrl, play });
+        });
+
+        mediaRecorder.stop();
+      });
+
+    resolve({ start, stop });
+  });
+
+const sleep = time => new Promise(resolve => setTimeout(resolve, time));
+
+// const recordStrike = async () => {
+async function recordStrike(button, dest) {
+
+  // Button stuff
+  console.log('start recording')
+  button.disabled = true;
+
+  const recorder = await recordAudio();
+  recorder.start();
+  await sleep(3000);
+  radSample = await recorder.stop();
+
+  // Button stuff
+  console.log('stop recording')
+  button.disabled = false;
+}
+
+async function playStrike(button, src) {
+
+  console.log('start playback');
+  button.disabled = true;
+
+  src.play();
+  await sleep(3000);
+
+  console.log('stop playback');
+  button.disabled = false;
+
+}
+
+var radSample;
+var latSample;
 
 $(function() {
   console.log('JQuery enabled');
 
-  // Create audio context
-  ctx = new AudioContext();
+  $('#recordRadial').click(function() {recordStrike(this, radSample);});
 
-  $('#recordRadial').click(function() {
-    console.log('clicked');
+  $('#playRadial').click(function() {playStrike(this, radSample);});
 
-
-    $(this).toggleClass('btn-danger');
-    // $(this).removeClass('btn-success');
-  });
 })
