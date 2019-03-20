f3e6c6b9cf234132c6baae855d865b7953fe7ba2
Matt Ford <mateoford@gmail.com>
2019-03-20 17:15:30 -0500

    Add HTTPS to SITEURL

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://bicyclewheel.info'
+SITEURL = 'https://bicyclewheel.info'
 RELATIVE_URLS = False
 
 DELETE_OUTPUT_DIRECTORY = True
